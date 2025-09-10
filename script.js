c580d96f094e273c446c120aacb4c933af9b6714
Javiera Vera <maripozazul030@gmail.com>
2025-09-09 23:27:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,78 +12,85 @@ d3.json("data.json").then(function(data) {
     return newObj
   })
 
-const filteredData = cleanedData.filter(d => d.YearPublished >= 2000 && d.ID <= 100000)
+  // Esto filtra los datos para que no sea tannn largo... igual lo hice de 50 barras
+const filteredData = cleanedData.filter(d => 
+    d.YearPublished >= 2000 &&
+    d.ID != null &&
+    d.ID >= 200 &&
+    d.ID <= 100000)
 
-const etiqueta = d3.select('body').append('div')
+ const margin = { top: 40, right: 40, bottom: 150, left: 80 }
+  const barWidth = 40 // ancho de cada barra
+  const width = filteredData.length * (barWidth + 10) // ancho dinámico segun cantidad de barras
+  const height = 580 - margin.top - margin.bottom // Margenes para que no se topen
+
+  const svg = d3.select(".chart") // Bendita sea en svg que no hay manera de hacer algo decente sin que lo mencionen en las referencias >:(
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+  const xScale = d3.scaleBand()
+    .domain(filteredData.map(d => d.Name))
+    .range([0, width])
+    .padding(0.3)
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(filteredData, d => d.UsersRated)])
+    .nice()
+    .range([height, 0])
+
+  const colorScale = d3.scaleSequential()
+    .domain(d3.extent(filteredData, d => d.YearPublished))
+    .interpolator(d3.interpolatePlasma) // Asi no tienen el mismo color
+
+  const tooltip = d3.select('body').append('div')
     .classed('etiqueta', true)
 
-// Todo para que no choquen los mendigos circulos >:(
-const radius = 10
-const idScale = d3.scaleLinear()
-  .domain(d3.extent(filteredData, d => d.ID)) // Se autoescala
-  .range([80 + radius, 1000 - radius])
-
-const yearScale = d3.scaleLinear()
-  .domain([2000, 2025])
-  .range([520 - radius, 20 + radius])
-const colorScale = d3.scaleSequential()
-  .domain(d3.extent(filteredData, d => d.YearPublished))
-  .interpolator(d3.interpolatePlasma)
-
-const ratedScale = d3.scaleSqrt()
-    .domain(d3.extent(filteredData, d => d.UsersRated))
-    .range([radius, radius])
-
-d3.select('.chart')
-  .selectAll('circle')
-  .data(filteredData)  //Alegria alegria que si funciona ahhhhhh
-  .join('circle')
-  .attr('r', d => ratedScale(d.UsersRated))
-  .attr('cx', d => idScale(d.ID))
-  .attr('cy', d => yearScale(d.YearPublished) + (Math.random() * 5 -2.5))
-  .attr('fill', d => colorScale(d.YearPublished))
-  .attr('opacity', 0.5)
-
-    .on('mouseenter', (e, d) => {
-      etiqueta
+  svg.selectAll("rect")
+    .data(filteredData)
+    .join("rect")
+    .attr("x", d => xScale(d.Name))
+    .attr("y", d => yScale(d.UsersRated))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - yScale(d.UsersRated))
+    .attr("fill", d => colorScale(d.YearPublished))
+    .attr("opacity", 0.8)
+    .on("mouseenter", (e, d) => {
+      tooltip
         .style('top', e.pageY + 5 + 'px')
         .style('left', e.pageX + 5 + 'px')
         .style('opacity', 1)
-        .html(`<p><strong>${d.Name}</strong><br>ID: ${d.ID}<br>Año: ${d.YearPublished}<br>Usuarios: ${d.UsersRated}</p>`)
+        .html(`
+          <p><strong>${d.Name}</strong></p>
+          <p>ID: ${d.ID}</p>
+          <p>Año: ${d.YearPublished}</p>
+          <p>Usuarios: ${d.UsersRated}</p>
+        `)
     })
-    .on('mouseout', () => {
-      etiqueta.style('opacity', 0)
-    })
+    .on("mouseout", () => tooltip.style('opacity', 0))
+
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .attr("text-anchor", "end")
+
+  svg.append("g")
+    .call(d3.axisLeft(yScale))
+
+  // Etiquetas de ejes para no morir en el intento
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 120)
+    .attr("text-anchor", "middle")
+    .text("Nombre del Juego")
+
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -50)
+    .attr("text-anchor", "middle")
+    .text("Usuarios Valorados")
 })
-
-const idScale = d3.scaleLinear().domain([0, 100000]).range([80, 1000])
-const yearScale = d3.scaleLinear().domain([2000, 2025]).range([520, 20])
-
-const xAxis = d3.axisBottom(idScale)
-    .tickValues([0, 20000, 40000, 60000, 80000, 100000])
-    .tickFormat(d3.format("d"))
-d3.select('.x-axis')
-    .attr('transform', 'translate(-20, 520)')
-    .call(xAxis)
-
-const yAxis = d3.axisLeft(yearScale)
-    .tickValues([2000, 2005, 2010, 2015, 2020, 2025])
-    .tickFormat(d3.format("d"))
-d3.select('.y-axis')
-    .attr('transform', 'translate(60, 0)')
-    .call(yAxis)
-
-d3.select('.chart')
-    .append('text')
-    .attr('x', 550)
-    .attr('y', 550)
-    .attr('text-anchor', 'middle')
-    .text('ID de Juego') 
-
-d3.select('.chart')
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('x', -250)
-    .attr('y', 20)
-    .attr('text-anchor', 'middle')
-    .text('Año de Publicación')  // Con tilde y todo :D
